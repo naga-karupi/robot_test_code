8fdad277bc4979414bed2c231efdad7751cceca5
naga <toritonsuibu0527@gmail.com>
2022-03-14 21:09:52 +0900

    delete binary file

--- pc_connection/matplot.cpp
+++ pc_connection/matplot.cpp
@@ -1,3 +1,15 @@
+/* 
+ * こんたくんのためにstd::vectorを解説
+ *
+ * 簡単言うと可変長配列
+ * 配列と同じような操作が可能
+ * vecというvectorがあったら
+ * vec.at(x);でx番目の要素にアクセス、配列でいうarr[x];みたいに扱える
+ * ちなみにvec[x];とも書ける
+ * vec.size();で配列の大きさを取れる sizeofなんて時代遅れだ
+ * 
+ */
+
 #include<matplotlib-cpp/matplotlibcpp.h>
 #include<vector>
 #include<cmath>
